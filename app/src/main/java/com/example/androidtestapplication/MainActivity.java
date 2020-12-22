e47beab17f6faea33d0f94af105daf037076484c
Suman Neupane <sumanmyon.sam@gmail.com>
2020-12-22 11:35:46 +0545

    New release

--- app/src/main/java/com/example/androidtestapplication/MainActivity.java
+++ app/src/main/java/com/example/androidtestapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.androidtestapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.distribute.Distribute;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "40079d62-7e47-46b6-9caa-53e11cc105ff",
+                Analytics.class, Crashes.class);
+        AppCenter.start(getApplication(), "40079d62-7e47-46b6-9caa-53e11cc105ff", Distribute.class);
 
         crashButton = findViewById(R.id.crashButton);
         reviewButton = findViewById(R.id.reviewButton);
